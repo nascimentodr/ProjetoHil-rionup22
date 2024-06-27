326d86d358a732592e6d769cad8560da3a57d17b
Adriano M <adriano.nascimento@academico.uncisal.edu.br>
2024-06-27 20:02:16 -0300

    criacao TesteCliente. Reorganizar código

--- src/teste/TesteCliente.java
+++ src/teste/TesteCliente.java
@@ -1,12 +1,166 @@
-/*
+
 package teste;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+import model.Cliente;
+import model.Venda;
+import service.ClienteService;
+
 
 import java.util.Scanner;
 
 public class TesteCliente {
     
-    public void opcaoCliente(){
-        }
+    
+    public void opcaoCliente() throws SQLException{
+            
+        int opcao, novaOpcao, id, idVenda;
+        String nome, email, endereco, cpf, telefone;
+    
+        
+        
+        Scanner teclado = new Scanner(System.in);
+        ClienteService clienteService = new ClienteService();
+
+        Cliente c = new Cliente("teste", "teste", "teste", "teste", "teste");
+        
+        
+            System.out.println("1: Listar clientes");
+            System.out.println("2: Adicionar um cliente");
+            System.out.println("3: Editar um cliente");
+            System.out.println("4: Deletar um cliente");
+            System.out.println("5: Recuperar cliente através da venda");
+            System.out.print("Opção: ");
+            novaOpcao = teclado.nextInt();
+            
+            // Opções relacionadas a clientes
+                switch(novaOpcao){
+                    case 1:
+                        System.out.println("Vai listar");
+
+                        ResultSet rs = clienteService.listar();
+
+                        while(rs.next()){
+                            System.out.print("Nº identificador: ");
+                            System.out.println(rs.getString("cliente_id"));
+                            System.out.print("Nome: ");
+                            System.out.println(rs.getString("nome"));
+                            System.out.print("Email: ");
+                            System.out.println(rs.getString("email"));
+                            System.out.print("Endereco: ");
+                            System.out.println(rs.getString("endereco"));
+                            System.out.print("CPF: ");
+                            System.out.println(rs.getString("cpf"));
+                            System.out.print("Telefone: ");
+                            System.out.println(rs.getString("telefone"));
+
+                            System.out.println("---------");
+                        }
+                        
+                        break;
+                     
+                    case 2:   // concluído
+                        System.out.println("Vai inserir");
+                        
+                        System.out.println("OK! Vamos precisar dos dados do cliente.");
+                        System.out.println("Nome do cliente: ");
+                        nome = teclado.next();
+                        System.out.println("Email: ");
+                        email = teclado.next();
+                        System.out.println("Endereco completo: ");
+                        endereco = teclado.next();
+                        System.out.println("CPF: ");
+                        cpf = teclado.next();   // pula direto para telefone.
+                                                //erro ao dar espaços.
+                        System.out.print("Telefone: ");
+                        telefone = teclado.next();
+                        
+                        c = new Cliente(nome, email, 
+                                endereco, cpf, telefone);
+                        
+                        clienteService.inserir(c);
+                        break;
+                        
+                        
+                        
+                        
+                    case 3: // concluído
+                        System.out.println("Vai editar");
+                        System.out.println("Qual o id do cliente que deseja editar? ");
+                        id = teclado.nextInt();
+                        System.out.println("Nome do cliente: ");
+                        nome = teclado.next();
+                        System.out.println("Email: ");
+                        email = teclado.next();
+                        System.out.println("Endereco completo: ");
+                        endereco = teclado.next();
+                        System.out.println("CPF: ");
+                        cpf = teclado.next();
+                        System.out.print("Telefone: ");
+                        telefone = teclado.next();
+                        
+                        c = new Cliente(nome, email, endereco, cpf, telefone);
+                        c.setClienteId(id);
+              
+                       
+                        clienteService.editar(c);
+                        
+                        
+                        
+                        break;
+                    case 4:
+                        System.out.println("Vai deletar cliente");
+                        
+                        System.out.println("Qual o id do cliente que deseja deletar? ");
+                        id = teclado.nextInt();
+                        c.setClienteId(id);
+                        clienteService.excluir(c);
+                        break;
+                        
+                        
+                    
+                    case 5:
+                        System.out.println("Vai encontrar cliente através da venda");
+                        System.out.println("Código id da venda: ");
+                        idVenda = teclado.nextInt();
+                        Venda v = new Venda(idVenda, null, null, null, 0);
+                        
+                        
+                        
+                        rs = clienteService.retornarClienteVenda(idVenda);
+                        
+                        while(rs.next()){
+                            System.out.print("Nº identificador: ");
+                            System.out.println(rs.getString("cliente_id"));
+                            System.out.print("Nome: ");
+                            System.out.println(rs.getString("nome"));
+                            System.out.print("Email: ");
+                            System.out.println(rs.getString("email"));
+                            System.out.print("Endereco: ");
+                            System.out.println(rs.getString("endereco"));
+                            System.out.print("CPF: ");
+                            System.out.println(rs.getString("cpf"));
+                            System.out.print("Telefone: ");
+                            System.out.println(rs.getString("telefone"));
+
+                            System.out.println("---------");
+
+                        }
+                        
+                        
+                        
+                        
+                        
+                        break;
+                    default:
+                        System.out.println("Opcao inválida");
+                }
+                
+        
+        
+        
     }
+    
 }
-*/
--- src/teste/TesteFinal.java
+++ src/teste/TesteFinal.java
@@ -48,151 +48,11 @@ public class TesteFinal {
         switch(opcao){
             
             case 1:
-                System.out.println("1: Listar clientes");
-                System.out.println("2: Adicionar um cliente");
-                System.out.println("3: Editar um cliente");
-                System.out.println("4: Deletar um cliente");
-                System.out.println("5: Recuperar cliente através da venda");
-                System.out.print("Opção: ");
-                novaOpcao = teclado.nextInt();
                 
-                
-                
-                // Opções relacionadas a clientes
-                switch(novaOpcao){
-                    case 1:
-                        System.out.println("Vai listar");
-
-                        ResultSet rs = clienteService.listar();
-
-                        while(rs.next()){
-                            System.out.print("Nº identificador: ");
-                            System.out.println(rs.getString("cliente_id"));
-                            System.out.print("Nome: ");
-                            System.out.println(rs.getString("nome"));
-                            System.out.print("Email: ");
-                            System.out.println(rs.getString("email"));
-                            System.out.print("Endereco: ");
-                            System.out.println(rs.getString("endereco"));
-                            System.out.print("CPF: ");
-                            System.out.println(rs.getString("cpf"));
-                            System.out.print("Telefone: ");
-                            System.out.println(rs.getString("telefone"));
-
-                            System.out.println("---------");
-
-                        }
-                        break;
-                        
-                        
-                        
-                    case 2:   // concluído
-                        System.out.println("Vai inserir");
-                        
-                        System.out.println("OK! Vamos precisar dos dados do cliente.");
-                        System.out.println("Nome do cliente: ");
-                        nome = teclado.next();
-                        System.out.println("Email: ");
-                        email = teclado.next();
-                        System.out.println("Endereco completo: ");
-                        endereco = teclado.next();
-                        System.out.println("CPF: ");
-                        cpf = teclado.next();   // pula direto para telefone
-                        
-                        System.out.print("Telefone: ");
-                        telefone = teclado.next();
-                        
-                        c = new Cliente(nome, email, 
-                                endereco, cpf, telefone);
-                        
-                        clienteService.inserir(c);
-                        break;
-                        
-                        
-                        
-                        
-                    case 3: // concluído
-                        System.out.println("Vai editar");
-                        System.out.println("Qual o id do cliente que deseja editar? ");
-                        id = teclado.nextInt();
-                        System.out.println("Nome do cliente: ");
-                        nome = teclado.next();
-                        System.out.println("Email: ");
-                        email = teclado.next();
-                        System.out.println("Endereco completo: ");
-                        endereco = teclado.next();
-                        System.out.println("CPF: ");
-                        cpf = teclado.next();
-                        System.out.print("Telefone: ");
-                        telefone = teclado.next();
-                        
-                        c = new Cliente(nome, email, endereco, cpf, telefone);
-                        c.setClienteId(id);
-              
-                       
-                        clienteService.editar(c);
-                        
-                        
-                        
-                        break;
-                    case 4:
-                        System.out.println("Vai deletar cliente");
-                        
-                        System.out.println("Qual o id do cliente que deseja deletar? ");
-                        id = teclado.nextInt();
-                        c.setClienteId(id);
-                        clienteService.excluir(c);
-                        break;
-                        
-                        
-                    
-                    case 5:
-                        System.out.println("Vai encontrar cliente através da venda");
-                        System.out.println("Código id da venda: ");
-                        idVenda = teclado.nextInt();
-                        Venda v = new Venda(idVenda, null, null, null, 0);
-                        
-                        
-                        
-                        rs = clienteService.retornarClienteVenda(idVenda);
-                        
-                        while(rs.next()){
-                            System.out.print("Nº identificador: ");
-                            System.out.println(rs.getString("cliente_id"));
-                            System.out.print("Nome: ");
-                            System.out.println(rs.getString("nome"));
-                            System.out.print("Email: ");
-                            System.out.println(rs.getString("email"));
-                            System.out.print("Endereco: ");
-                            System.out.println(rs.getString("endereco"));
-                            System.out.print("CPF: ");
-                            System.out.println(rs.getString("cpf"));
-                            System.out.print("Telefone: ");
-                            System.out.println(rs.getString("telefone"));
-
-                            System.out.println("---------");
-
-                        }
-                        
-                        
-                        
-                        
-                        
-                        break;
-                    default:
-                        System.out.println("Opcao inválida");
-                }
+                TesteCliente tc = new TesteCliente();
+                tc.opcaoCliente();
                 break;
                 
-                
-                
-                
-                
-                // ACABAM AQUI OS AS OPÇÕES RELACIONADAS AOS CLIENTES
-
- 
-            
-             
             case 2:
                 System.out.print("Operações do produto");
                 break;
@@ -208,13 +68,34 @@ public class TesteFinal {
             
             default:
                 System.out.println("Opção incorreta.");
+                /*
+                System.out.println("1: Listar clientes");
+                System.out.println("2: Adicionar um cliente");
+                System.out.println("3: Editar um cliente");
+                System.out.println("4: Deletar um cliente");
+                System.out.println("5: Recuperar cliente através da venda");
+                System.out.print("Opção: ");
+                
+                novaOpcao = teclado.nextInt();
+                */
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                // ACABAM AQUI OS AS OPÇÕES RELACIONADAS AOS CLIENTES
+
+ 
+            
                        
         }
-        
-    }
 }
 
-
+}
 
 
 
